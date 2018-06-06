965cd9d2398f41ca9966f60acc00e65ed13e8e3e
Alexander Korzun <korzun.sas@mail.ru>
2018-06-06 19:51:14 +0300

    Replaced sudo with fakeroot in make-pak.sh

--- make-pak.sh
+++ make-pak.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ -z "$1" ]; then
     echo "Usage: $0 <package_name>"
     echo "Creates a Pak2 package"
@@ -14,10 +16,10 @@ echo 'nani' > pak2-package/.pak2/name
 echo '' > pak2-package/.pak2/deps
 
 ./build.py
-sudo install -m 0755 -o root -g root nani pak2-package/files/usr/bin
+install -m 0755 nani pak2-package/files/usr/bin
 
 cd pak2-package
-tar -cjvf package.pak files .pak2
+fakeroot tar -cjvf package.pak files .pak2
 cd ..
 mv -i pak2-package/package.pak "$1"
 rm -rf pak2-package
